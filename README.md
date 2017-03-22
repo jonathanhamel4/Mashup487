910d348f7cea94113477e6bc309fd6429e946d74
Mohammed Naser <mnaser@vexxhost.com>
2017-03-22 18:04:54 -0400

    setup env with gulp

---
+++ README.md
@@ -0,0 +1,12 @@
+## Getting Started
+In order to get started, clone the repository locally and then make sure that
+you have the `npm` dependencies:
+
+    npm install
+
+Once all the dependencies are installed, you can start up the project by running
+the following command:
+
+    npm start
+
+The application will then be accessible at [`http://localhost:3000`](http://localhost:3000)
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,7 @@ var igApi = require('../api/instagram');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Mashup Service' });
-  igApi.getMediasByTag("montreal");
-  res.send(200);
-});
-
-/* GET home page. */
-router.post('/', function(req, res, next) {
-  // res.render('index', { title: 'Mashup Service' });
-  console.log(JSON.stringify(res.body, null, 2));
-  res.send(200);
+  res.render('index', { title: 'Mashup Service' });
 });
 
 module.exports = router;
