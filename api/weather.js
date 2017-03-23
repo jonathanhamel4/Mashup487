e946a886409a05d09921b18ff957ddaf0ea986e3
Jonathan Hamel <jonathanhamel4@gmail.com>
2017-03-22 23:16:44 -0400

    Added forecast

--- api/weather.js
+++ api/weather.js
@@ -4,11 +4,21 @@ const wConfig = require('../config/config.json').weather;
 
 module.exports = {
   currentWeather: currentWeather
+  forecast: forecast
 }
 
 function currentWeather(city){
   return rp('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + wConfig.client_id + "&unit=metric")
     .then((data) => {
       console.log(data);
+      return data;
+    });
+}
+
+function forecast(city){
+  return rp('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + wConfig.client_id + "&unit=metric")
+    .then((data) => {
+      console.log(data);
+      return data;      
     });
 }
