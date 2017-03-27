1bb9761a2a1788183ee33d776bbffeae6e0aa9f0
Jonathan Hamel <jonathanhamel4@gmail.com>
2017-03-26 21:04:00 -0400

    Added twitter 3 trending topics

--- api/twitter.js
+++ api/twitter.js
@@ -8,9 +8,39 @@ module.exports = {
   getTrends: getTrends
 }
 
+/*
+ params = lat & long
+*/
 function getTrends(){
-    console.log(tConfig)
-  return client.get('/trends/closest.json?lat=37.781157&long=-122.400612831116', function(error, trends, response){
-        console.log(trends);
+  return getClosest().then((woeid) => {
+    if(woeid){
+      return getTrendingTweets(woeid);
+    } else {
+      return null;
+    }
+  }).catch(err => {
+    console.log(err);
   })
+}
+
+function getClosest(){
+  return new Promise((resolve, reject) => {
+    client.get('/trends/closest.json', {lat: 45.500299, long:-73.571646}, function(error, trends, response){
+      var woeid = trends.length > 0 ? trends[0].woeid : null;
+      resolve(woeid);
+    });
+  });
+}
+
+function getTrendingTweets(woeid){
+  return new Promise((resolve, reject) => {
+    console.log(woeid);
+    client.get("/trends/place.json", {id: woeid}, function(error, tweets, response){
+      if(tweets.length && tweets[0].trends && tweets[0].trends.length){
+        resolve(tweets[0].trends.slice(0, 3));
+      } else {
+        resolve([]);
+      }
+    });
+  });
 }
