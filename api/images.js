f45df2f5730caf1e679edabd185d15bf1562c91c
FreshCut <TeamD@nest.encs.concordia.ca>
2017-04-05 16:15:00 -0400

    Fixed images with variable

--- api/images.js
+++ api/images.js
@@ -7,7 +7,7 @@ module.exports = {
 
 function getImages(imgQuery){
   var query = encodeURI(imgQuery);
-  var url = "https://www.googleapis.com/customsearch/v1?num=3&searchType=image&q=montreal&cx=" + iConfig.cx + "&key=" + iConfig.api_key +"&alt=json";
+  var url = "https://www.googleapis.com/customsearch/v1?num=3&searchType=image&q=" + query + "&cx=" + iConfig.cx + "&key=" + iConfig.api_key +"&alt=json";
   return rp.get(url)
   .then((results) => {
     return JSON.parse(results).items;
